7ad1dc15df35529327ea85f04d2375afbe20acb6
Alvaro Centeno <alvaroc0689@gmail.com>
2020-06-01 19:46:19 -0700

    added minus function

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,5 @@
-const { add } = require("../util/util");
+const { add, minus } = require("../util/util");
+
 
 describe('Add', () => {
     describe('Success', () => {
@@ -20,6 +21,16 @@ describe('Add', () => {
 
 describe('Minus', () => {
     describe('Success', () => {
-
-    })
-})
+        it('Should return the less of the 2 past arguments', () => {
+            expect(minus(3, 2)).toBe(1);
+        });
+    });
+    describe('Failure', () => {
+        it("Should throw an error if 1st argument isn't a num", () => {
+            expect(() => minus('2', 2)).toThrowError();
+        });
+        it("Should throw an error if 1st argument isn't a num", () => {
+            expect(() => minus(2, '2')).toThrowError();
+        });
+    });
+});
